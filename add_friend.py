0334eb1047d8700de269f7859089dbeec3c902a6
Anant Prakash Singh <69471106+TheViking733n@users.noreply.github.com>
2022-06-12 18:31:56 +0530

    Added fetch user friends feature

--- add_friend.py
+++ add_friend.py
@@ -31,7 +31,7 @@ def take_handle_input():
 
     with open(INPUT_FILE_NAME, "r") as f:
         handles = f.readlines()
-    return list(set(h.strip() for h in handles))
+    return [h.strip() for h in handles]
 
 
 def wait_for_login(browser):
@@ -64,15 +64,47 @@ if __name__ == "__main__":
     driver.get(login_url)
     wait_for_login(driver)
 
+
+    # Fetching handles that are already friend of this user
+    print("\nFetching those handles which are already in your friends...\n")
+    driver.get("https://codeforces.com/friends")
+    sleep(.5)
+    if driver.current_url.startswith("https://codeforces.com/enter"):
+        print("\n\n\nUser didn't Logged in")
+        print("\nProcess Halted!")
+        exit(1)
+    
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    friends_list = []
+    try:
+        table = soup.find("div", {"class": "datatable"}).find("tbody")
+        rows = table.find_all("tr")
+        for row in rows:
+            friend_handle = row.find_all("td")[1].text
+            friends_list.append(friend_handle)
+    except:
+        pass
+
+    # Removing all handles which are already friend of the user
+    not_friends = []
+    for handle in handles:
+        if handle not in friends_list and handle not in not_friends and handle != "":
+            not_friends.append(handle)
+
+    # # For debugging
+    # print(handles)        # Handles in input file
+    # print(friends_list)   # List of friends of user
+    # print(not_friends)    # List of handles which are not friends of user
+
     # Adding friends
     new_friends = []
-    for handle in handles:
+    for handle in not_friends:
         try:
             driver.get("https://codeforces.com/profile/{}".format(handle))
             page_source = driver.page_source
             soup = BeautifulSoup(page_source, 'lxml')
             h1 = soup.find("h1")
-
+            
             if "Click to add to friends" in str(h1.img):
                 print("\nAdding {} to friends".format(handle))
                 star = driver.find_element(By.TAG_NAME, "h1").find_element(By.TAG_NAME, "img")
@@ -80,12 +112,12 @@ if __name__ == "__main__":
                 sleep(.2)
                 new_friends.append(handle)
         except Exception as e:
-            print(f"Error occured while fetching {handle}")
-            print(e)
+            print(f"Handle doesn't exist: {handle}")
+            # print(e)
     
     print("\n\n\n\n")
-    print(*new_friends, sep="\n")
     print(f"\nSuccess: {len(new_friends)} new friends added !")
+    print(*new_friends, sep="\n")
     print("\nDone...")
     
     # Redirecting to My Friends page
